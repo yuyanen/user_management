30df644b35722bb93f66b70a927114cba996e62d
James <jamesyuyanen@gmail.com>
2024-06-30 23:10:16 +0800

    update shell script

--- run.sh
+++ run.sh
@@ -1,6 +1,26 @@
-# Build Docker images using docker-compose (with --no-cache option)
+#!/bin/bash
+
+
+# Build Angular frontend
+echo "Building Angular frontend..."
+(cd user-management-frontend && docker build -t user-management-frontend .)
+
+# Build Email service
+echo "Building Spring Boot backend..."
+(cd email-service && ./mvnw package -DskipTests)
+(cd email-service && docker build -t email-service .)
+
+# Build User management
+echo "Building Spring Boot backend..."
+(cd user-management && ./mvnw package -DskipTests)
+(cd user-management && docker build -t user-management .)
+
+# Build Docker images using docker-compose with --no-cache option
+echo "Building Docker images with --no-cache option..."
 docker-compose build --no-cache
 
-# Start Docker containers in detached mode
+# Start containers using Docker Compose
+echo "Starting containers..."
 docker-compose up -d
 
+echo "Application deployed successfully."
