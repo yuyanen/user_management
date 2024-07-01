fe2d4ada444fae5eb523ca96d0e166f7a9080714
James <jamesyuyanen@gmail.com>
2024-07-01 15:08:26 +0800

    Add REST API

--- user-management/src/main/java/com/example/user_management/controller/UserController.java
+++ user-management/src/main/java/com/example/user_management/controller/UserController.java
@@ -5,6 +5,9 @@ import com.example.user_management.model.User;
 import com.example.user_management.service.UserServiceImpl;
 import com.mailjet.client.errors.MailjetException;
 import com.mailjet.client.errors.MailjetSocketTimeoutException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -38,6 +41,12 @@ public class UserController {
     }
 
 
+    @Operation(summary = "create user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "success"),
+            @ApiResponse(responseCode = "400", description = "not valid request"),
+            @ApiResponse(responseCode = "500", description = "server error")
+    })
     @PostMapping("/register")
     public ResponseEntity<User> createUser(@Valid @RequestBody User user) throws MessagingException, IOException, MailjetSocketTimeoutException, MailjetException {
         User createdUser = userService.createUser(user);
@@ -45,6 +54,12 @@ public class UserController {
     }
 
 
+    @Operation(summary = "update user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "success"),
+            @ApiResponse(responseCode = "400", description = "not valid request"),
+            @ApiResponse(responseCode = "500", description = "server error")
+    })
     @PutMapping("/{id}")
     public ResponseEntity<User> updateUser(@PathVariable("id") Long id, @Valid @RequestBody User user) {
         if (!userService.getUserById(id).isPresent()) {
@@ -56,6 +71,12 @@ public class UserController {
     }
 
 
+    @Operation(summary = "delete user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "success"),
+            @ApiResponse(responseCode = "400", description = "not valid request"),
+            @ApiResponse(responseCode = "500", description = "server error")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable("id") Long id) {
         if (!userService.getUserById(id).isPresent()) {
@@ -66,6 +87,12 @@ public class UserController {
     }
 
 
+    @Operation(summary = "deactivate user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "success"),
+            @ApiResponse(responseCode = "400", description = "not valid request"),
+            @ApiResponse(responseCode = "500", description = "server error")
+    })
     @PutMapping("/{id}/deactivate")
     public ResponseEntity<User> deactivateUser(@PathVariable("id") Long id) {
         User deactivatedUser = userService.deactivateUser(id);
@@ -73,6 +100,12 @@ public class UserController {
     }
 
 
+    @Operation(summary = "activate user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "success"),
+            @ApiResponse(responseCode = "400", description = "not valid request"),
+            @ApiResponse(responseCode = "500", description = "server error")
+    })
     @PutMapping("/{id}/activate")
     public ResponseEntity<User> activateUser(@PathVariable("id") Long id) {
         Optional<User> userOptional = userService.getUserById(id);
